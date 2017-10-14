3621fa4725333861785ea7fcb2701d41854beb4e
daheige <zhuwei313@hotmail.com>
2017-10-14 23:21:00 +0800

    es6转换为es5

--- hgdemo/es6/hg_test.js
+++ hgdemo/es6/hg_test.js
@@ -1,6 +1,8 @@
-require([], function() {
-    let arr = [1, 2, 3];
-    for (let i = 0; i < arr.length; i++) {
+require([], function () {
+    var arr = [1, 2, 3];
+    for (var i = 0; i < arr.length; i++) {
         console.log(i);
     }
+
+    console.log("daheige auto babel,hello");
 });
